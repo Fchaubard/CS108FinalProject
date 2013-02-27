fda5c5aa7a2ecbdacfbe4c829f02f260ff661e89
Francois Chaubard <fchaubard@gmail.com>
2013-02-26 23:23:11 -0800

    Added picture_response_question

--- model/PictureResponse.java
+++ model/PictureResponse.java
@@ -1,8 +1,11 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 public class PictureResponse implements Question {
 
@@ -11,14 +14,93 @@ public class PictureResponse implements Question {
 	private Set<String> answers;
 	private int qID;
 	
-	public PictureResponse(String picture, Set<String> ans, int id) {
-		url = picture;
+
+	public int getqID() {
+		return qID;
+	}
+
+	public void setqID(int qID) {
+		this.qID = qID;
+	}
+
+	public PictureResponse(String url, Set<String> ans, int numAnswers, Connection con) { // pushes to database
+		Statement stmt;
 		
-		for(String s : ans) {
-			answers.add(s);
+		this.url = url;
+		this.answers = ans;
+		
+		try {
+			stmt = con.createStatement();
+			StringBuilder sqlString = new StringBuilder("INSERT INTO picture_response_question VALUES(null,\"");
+			sqlString.append(url);
+			sqlString.append("\",\" ");
+			for (String string : ans) {
+				sqlString.append(string);
+				sqlString.append(" &&& ");
+			}
+			sqlString.append("\" ");
+			
+			System.out.print(sqlString.toString());
+			ResultSet resultSet = stmt.executeQuery(sqlString.toString());
+			
+			stmt = con.createStatement();
+			sqlString = new StringBuilder("SELECT * FROM picture_response_question WHERE statement=\"\"");
+			sqlString.append(url);
+			sqlString.append("\" ");
+			
+			System.out.print(sqlString.toString());
+			resultSet = stmt.executeQuery(sqlString.toString());
+			
+			
+			while (resultSet.next()) {
+				this.setqID(resultSet.getInt(0)); // will always be the last one
+			}
+			
+			
+			
+			
+		}catch(Exception e){
+			
 		}
 		
-		qID = id;
+		
+		
+	}
+
+	public PictureResponse(int id, Connection con) { // pulls from database
+		generate(id, con);
+	}
+	
+	public void generate(int id, Connection con) {
+		setqID(id);
+		Statement stmt;
+		try {
+			stmt = con.createStatement();
+			StringBuilder sqlString = new StringBuilder("SELECT * FROM picture_response_question WHERE id=\"\"");
+			sqlString.append(id);
+			sqlString.append("\" ");
+			
+			System.out.print(sqlString.toString());
+			ResultSet resultSet = stmt.executeQuery(sqlString.toString());
+			
+			String ans = new String();
+			while (resultSet.next()) {
+				url = resultSet.getString("url");
+				ans = resultSet.getString(2);
+				
+			}
+			
+			StringTokenizer tokenizer = new StringTokenizer(ans, "&&&");
+			while(tokenizer.hasMoreTokens()) {
+				answers.add(tokenizer.nextToken());
+			}
+			
+			
+			
+		}catch(Exception e){
+			
+		}
+		
 	}
 
 	public String getURL() {
@@ -52,12 +134,6 @@ public class PictureResponse implements Question {
         html.append("\" />");
 		
 		return html.toString();
-	}
-
-	@Override
-	public void generate(int id, Connection con) {
-		// TODO Auto-generated method stub
-		
 	}
 
 
