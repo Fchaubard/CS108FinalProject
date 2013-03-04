dbf63e73539e109609d639fef0713076f89d8437
Sean Stanko <ststanko@stanford.edu>
2013-03-03 18:38:33 -0800

    Third times the charm?

--- src/Servlets/FriendManagementServlet.java
+++ src/Servlets/FriendManagementServlet.java
@@ -30,7 +30,6 @@ package Servlets;
     	     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
     	     */
     	    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	    	S
     	    }
 
     	    /**
