2cf9a592fce1df9dd4b02d9428e74bb907ada5e4
Ramon Iglesias <ramon.d.iglesias@gmail.com>
2013-03-07 17:53:25 -0800

    This should work.... HTML Helpers will be a bunch of static function to aid in UI desing;

---
+++ src/helpers/HTMLHelper.java
@@ -0,0 +1,16 @@
+package helpers;
+
+import java.util.*;
+
+
+public class HTMLHelper {
+	
+	private HTMLHelper(){
+		
+	}
+	
+	public static String printHeader(){
+		return "<div class=header><a class=header href=\"GuestHome.jsp\">Moses-Foe Quiz Project</a></div><div style=\"min-height:40px\"></div>";
+	}
+	
+}
