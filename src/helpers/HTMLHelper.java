4dc096d8a82455fd68e9d8a9941b569f4826d655
Ramon Iglesias <ramon.d.iglesias@gmail.com>
2013-03-08 14:05:42 -0800

    more stuff

--- src/helpers/HTMLHelper.java
+++ src/helpers/HTMLHelper.java
@@ -10,7 +10,7 @@ public class HTMLHelper {
 	}
 	
 	public static String printHeader(){
-		return "<div class=header><ul class=header><li class=header style=\"float:left\"><a class=header href=\"HomePage\">Moses-Foe Quiz Project</a></li><li class=header><a class=header href=\"QuizCatalogServlet\">Take a Quiz!</a></li><li class=header><a class=header href=\"http://en.wikipedia.org/wiki/Special:Random\">Fun Fact</a></li></ul></div><div style=\"min-height:40px\"></div>";
+		return "<div class=header><ul class=header><li class=header style=\"float:left\"><a class=header href=\"HomePage\">Moses-Foe Quiz Project</a></li><li class=header><a class=header href=\"QuizCatalogServlet\">Take a Quiz!</a></li><li class=header><a class=header href=\"http://en.wikipedia.org/wiki/Special:Random\">Fun Fact</a></li></ul></div><div style=\"min-height:80px\"></div>";
 	}
 	
 	public static String printCSSLink(){
