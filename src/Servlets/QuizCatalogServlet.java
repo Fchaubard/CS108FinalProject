c71f9c0b7bd17c006a21f54e33835d7b622e0206
Sean Stanko <ststanko@stanford.edu>
2013-03-11 15:46:59 -0700

    Extended quiz search

--- src/Servlets/QuizCatalogServlet.java
+++ src/Servlets/QuizCatalogServlet.java
@@ -37,14 +37,25 @@ public class QuizCatalogServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Connection con = (Connection) request.getServletContext().getAttribute("connect");
+		AccountManager am = (AccountManager) request.getServletContext().getAttribute("accounts");
 		Statement stmt;
 		ResultSet rs;
 		try {
 			stmt = (Statement) con.createStatement();
 			String searchQuizes = "Select quiz_id, name, category, creator_id from quiz";
 			if (request.getParameter("search") != null && request.getParameter("search").length() > 0) {
-				searchQuizes += " where name like \"%"+request.getParameter("search")+"%\"";
+				String searchBy = request.getParameter("type");
+				String searchFor = request.getParameter("search");
+				if (searchBy.equals("creator_id")) {
+					Account author =  am.getAccount(searchFor);
+					if (author != null) searchFor = " = " + author.getId();
+					else searchFor = " = -1";
+				} else {
+					searchFor = " like \"%"+searchFor+"%\"";
+				}
+				searchQuizes += " where "+searchBy + searchFor;
 			}
+			System.out.println(searchQuizes);
 			rs = stmt.executeQuery(searchQuizes);
 			response.setContentType("text/html");
 	    	PrintWriter out = response.getWriter();
@@ -57,11 +68,17 @@ public class QuizCatalogServlet extends HttpServlet {
 	  
 	    	out.println("<form action=\"QuizCatalogServlet\" method=\"get\">");
 	    	out.println("Search Quizes: <input type=\"text\" name=\"search\"/>");
+	    	//out.println("<input type=\"hidden\" name=\"type\"/ value=\"name\">");
+	    	out.println("<select name = \"type\">");
+	    	out.println("<option value = \"name\">By quiz name</option>");
+	    	out.println("<option value = \"category\">By category</option>");
+	    	out.println("<option value = \"creator_id\">By author</option>");
+	    	out.println("</select>");
 	    	out.println("<input type=\"submit\" value=\"Search\"/>");
 			out.println("</form>");
 			out.println("<ul class=boxlisting>");
 			while (rs.next()) {
-				AccountManager am = (AccountManager) request.getServletContext().getAttribute("accounts");
+				
 				String name = rs.getString("name");
 				int id = rs.getInt("quiz_id");
 				String cat = rs.getString("category");
